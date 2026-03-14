ca47790a4b06db5ac9cf3f2d41904c7467b770a5
adetunjibadmus398-droid <adetunjibadmus398@gmail.com>
2026-03-14 17:30:50 -0400

    Create TrainSchedulerApplication.java

---
+++ TrainSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.train.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainSchedulerApplication.class, args);
+    }
+
+}
